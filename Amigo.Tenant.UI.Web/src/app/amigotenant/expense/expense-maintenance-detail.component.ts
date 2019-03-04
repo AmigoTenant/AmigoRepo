d9975417a63e11294a46e86bd7b93ffec99b55f6
Augusto Prado <mfpm5@hotmail.com>
2019-03-04 18:12:01 -0500

    Expense detail Validations

--- Amigo.Tenant.UI.Web/src/app/amigotenant/expense/expense-maintenance-detail.component.ts
+++ Amigo.Tenant.UI.Web/src/app/amigotenant/expense/expense-maintenance-detail.component.ts
@@ -1,13 +1,13 @@
-import {TranslateService} from '@ngx-translate/core';
-import {ExpenseDataService} from './expense-data.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ExpenseDataService } from './expense-data.service';
 import { ExpenseRegisterRequest } from './dto/expense-register-request';
 import { Component, AfterViewInit, EventEmitter, OnInit, OnDestroy, ElementRef, ViewChildren, Input, Output } from '@angular/core';
-import { Http, Jsonp, URLSearchParams} from '@angular/http';
+import { Http, Jsonp, URLSearchParams } from '@angular/http';
 import { FormGroup, FormBuilder, Validators, FormControl, ReactiveFormsModule, Form, FormControlName } from '@angular/forms';
 import { EntityStatusDTO, HouseDTO } from './../../shared/api/services.client';
 import { GridDataResult, PageChangeEvent, SelectionEvent } from '@progress/kendo-angular-grid';
 import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'angular-2-dropdown-multiselect';
-import { ConfirmationList, Confirmation } from  '../../model/confirmation.dto';
+import { ConfirmationList, Confirmation } from '../../model/confirmation.dto';
 import { ListsService } from '../../shared/constants/lists.service';
 import { EntityStatusClient, GeneralTableClient } from '../../shared/api/services.client';
 import { EnvironmentComponent } from '../../shared/common/environment.component';
@@ -22,8 +22,8 @@ import { ExpenseDetailRegisterRequest } from './dto/expense-detail-register-requ
 declare var $: any;
 
 @Component({
-  selector: 'at-expense-maintenance-detail',
-  templateUrl: './expense-maintenance-detail.component.html'
+    selector: 'at-expense-maintenance-detail',
+    templateUrl: './expense-maintenance-detail.component.html'
 })
 
 export class ExpenseMaintenanceDetailComponent extends EnvironmentComponent implements OnInit, AfterViewInit {
@@ -53,45 +53,24 @@ export class ExpenseMaintenanceDetailComponent extends EnvironmentComponent impl
     contractId: any;
 
     constructor(
-            private route: ActivatedRoute,
-            private router: Router,
-            private gnrlTableDataService: GeneralTableClient,
-            private expenseDataService: ExpenseDataService,
-            private formBuilder: FormBuilder,
-            private translate: TranslateService,
-            private masterDataService: MasterDataService
-        ) {
+        private route: ActivatedRoute,
+        private router: Router,
+        private gnrlTableDataService: GeneralTableClient,
+        private expenseDataService: ExpenseDataService,
+        private formBuilder: FormBuilder,
+        private translate: TranslateService,
+        private masterDataService: MasterDataService
+    ) {
         super();
-
-        // Observable.forkJoin([
-        //     this.translate.get('common.requiredField'),
-        //     this.translate.get('common.alphadashmax12'),
-        //     this.translate.get('common.maxLength', { value: 50 })
-        //   ]).subscribe((messages: string[]) => this.buildMessages(...messages));
-        //   this.genericValidator = new GenericValidator(this.validationMessages);
     }
 
-    // buildMessages(required?: string, notvalid?: string, maxlength?: string) {
-    //     this.validationMessages = {
-    //       conceptId: {
-    //         required: required
-    //       },
-    //       applyTo: {
-    //         required: required
-    //       },
-    //       subTotalAmount: {
-    //         required: required
-    //       }
-    //     };
-    // }
-
     ngOnInit() {
-        debugger;
         this.buildForm();
+        this.buildValidator();
         this.initializeForm();
         this.expenseDetailForm.patchValue(this.inputSelectedExpenseDetail);
 
-        if (this.inputSelectedExpenseDetail === undefined || this.inputSelectedExpenseDetail ===  null || 
+        if (this.inputSelectedExpenseDetail === undefined || this.inputSelectedExpenseDetail === null ||
             this.inputSelectedExpenseDetail.expenseDetailId === undefined) {
             this.flgEdition = 'N';
         } else {
@@ -99,28 +78,58 @@ export class ExpenseMaintenanceDetailComponent extends EnvironmentComponent impl
         }
     }
 
+    buildValidator() {
+        Observable.forkJoin([
+            this.translate.get('common.requiredField'),
+            this.translate.get('common.maxLength', { value: 500 }),
+            this.translate.get('common.notValidFormat')
+        ]).subscribe((messages: string[]) => this.buildMessages(...messages));
+        this.genericValidator = new GenericValidator(this.validationMessages);
+    }
+
+    buildMessages(required?: string, maxlength?: string, notvalid?: string) {
+        this.validationMessages = {
+            conceptId: {
+                required: required
+            },
+            applyTo: {
+                required: required
+            },
+            subTotalAmount: {
+                required: required
+            },
+            tax: {
+                required: required
+            },
+            totalAmount: {
+                required: required
+            },
+            tenantId: {
+                required: required
+            }
+        };
+    }
+
 
     buildForm() {
         this.expenseDetailForm = this.formBuilder.group({
             expenseDetailId: [null],
             conceptId: [null, [Validators.required]],
-            quantity: [null, [Validators.required]],
+            quantity: [1, [Validators.required]],
             subTotalAmount: [null, [Validators.required]],
             applyTo: [null, [Validators.required]],
-            tenantId: [null],
+            tenantId: [null, [Validators.required]],
             remark: [null],
-            tax: [null],
-            totalAmount: [null],
+            tax: [null, [Validators.required]],
+            totalAmount: [null, [Validators.required]],
             expenseId: [null],
             expenseDetailStatusId: [null]
-         });
+        });
     }
 
     getExpenseDetailByExpenseDetailId(id): void {
-
         this.expenseDataService.getExpenseDetailByExpenseDetailId(id).subscribe(
             response => {
-                debugger;
                 let dataResult: any = new ResponseListDTO(response);
                 this.model = dataResult.dat;
                 this.expenseDetailForm.patchValue(this.model);
@@ -139,36 +148,41 @@ export class ExpenseMaintenanceDetailComponent extends EnvironmentComponent impl
 
     acceptDetail(): void {
         debugger;
-        if (this.isValidData()) {
-
-            const model = new ExpenseDetailRegisterRequest();
-            Object.assign(model, this.expenseDetailForm.value);
-
-            if (this.flgEdition === 'N') {
-                this.expenseDataService.saveExpenseDetail(model)
-                    .subscribe(r => {
-                        let result = new ResponseListDTO(r);
-                    }).add(r => {
-                        debugger;
-                        this.onCancelDetail();
-                    });
-
-            } else {
-                //UPDATE
-                this.expenseDataService.updateExpenseDetail(model)
-                    .subscribe(r => {
-                        let result = new ResponseListDTO(r);
-                    }).add(r => {
-                        this.onCancelDetail();
-                    });
-            }
+        //this.setRutValidators();
+        if (!this.expenseDetailForm.valid) {
+            this.showErrors(true);
+            return;
         }
+        this.displayMessage = {};
+
+        const model = new ExpenseDetailRegisterRequest();
+        Object.assign(model, this.expenseDetailForm.value);
+
+        if (this.flgEdition === 'N') {
+            this.expenseDataService.saveExpenseDetail(model)
+                .subscribe(r => {
+                    let result = new ResponseListDTO(r);
+                }).add(r => {
+                    debugger;
+                    this.onCancelDetail();
+                });
+
+        } else {
+            //UPDATE
+            this.expenseDataService.updateExpenseDetail(model)
+                .subscribe(r => {
+                    let result = new ResponseListDTO(r);
+                }).add(r => {
+                    this.onCancelDetail();
+                });
+        }
+
     }
 
     ////===========
     ////DELETE
     ////===========
-    
+
     //public deleteMessage: string = "Are you sure to delete this Lease?";
     //deviceToDelete: any; //DeleteDeviceRequest;
 
@@ -199,7 +213,6 @@ export class ExpenseMaintenanceDetailComponent extends EnvironmentComponent impl
 
 
     cancel(): void {
-
         //this.router.navigate(['expense/edit/1']);
     }
 
@@ -234,79 +247,104 @@ export class ExpenseMaintenanceDetailComponent extends EnvironmentComponent impl
     //             }
     //         });
     // }
-    
 
-    isValidData(): boolean {
-        let isValid = true;
-        //this.resetFormError();
-        //if (this.model.tenantId == undefined || this.model.tenantId == 0 || this.model.tenantId == null) {
-        //    this._formError.tenantError = true;
-        //    isValid = false;
-        //}
-        return isValid;
-    }
+    // isValidData(): boolean {
+    //     let isValid = true;
+    //     //this.resetFormError();
+    //     //if (this.model.tenantId == undefined || this.model.tenantId == 0 || this.model.tenantId == null) {
+    //     //    this._formError.tenantError = true;
+    //     //    isValid = false;
+    //     //}
+    //     return isValid;
+    // }
 
 
 
     //EXPENSE DETAIL METHODS
 
-    
+
 
     ngAfterViewInit() {
         // Watch for the blur event from any input element on the form.
         // const controlBlurs: Observable<any>[] = this.formInputElements
         //   .map((formControl: ElementRef) => Observable.fromEvent(formControl.nativeElement, 'blur'));
-    
+
         // // Merge the blur event observable with the valueChanges observable
         // this.validationSubscription = Observable.merge(this.expenseDetailForm.valueChanges, ...controlBlurs)
         // .debounceTime(800).subscribe(value => this.showErrors());
-      }
-    
-      private showErrors(force = false) {
+    }
+
+    private showErrors(force = false) {
         this.displayMessage = this.genericValidator.processMessages(this.expenseDetailForm, force);
-      }
-    
-    //   _currentTenant: any;
+    }
 
-    //   getTenant = (item) => {
-    //       if (item != null && item != undefined) {
-    //           //this.model.tenantId = item.tenantId;
-    //           //this.model.fullName = item.fullName;
-    //           this._currentTenant = item;
-    //       }
-    //       else {
-    //           //this.model.tenantId = 0;
-    //           //this.model.fullName = item.username;
-    //           this._currentTenant = undefined;
-    //       }
-    //   };
+    _currentTenant: any;
 
-      _listApplyTo: any[];
-      getApplyTo(): void {
-          this.gnrlTableDataService.getGeneralTableByTableNameAsync("ApplyTo")
-              .subscribe(res => {
+    getTenant = (item) => {
+        if (item != null && item != undefined) {
+            //this.model.tenantId = item.tenantId;
+            //this.model.fullName = item.fullName;
+            this._currentTenant = item;
+            this.expenseDetailForm.get('tenantId').setValue(item.tenantId);
+        }
+        else {
+            //this.model.tenantId = 0;
+            //this.model.fullName = item.username;
+            this._currentTenant = undefined;
+            this.expenseDetailForm.get('tenantId').setValue(null);
+        }
+    };
 
-                  var dataResult: any = res;
-                  this._listApplyTo = [];
-                  for (var i = 0; i < dataResult.value.data.length; i++) {
-                      this._listApplyTo.push({
-                          "applyToId": dataResult.value.data[i].generalTableId,
-                          "name": dataResult.value.data[i].value
-                      });
-                  }
-              });
-      }
+    _listApplyTo: any[];
+    getApplyTo(): void {
+        this.gnrlTableDataService.getGeneralTableByTableNameAsync("ApplyTo")
+            .subscribe(res => {
 
-      getConceptByTypes(): void {
-          this.masterDataService.getConceptsByTypeIdList([31, 29])
-              .subscribe(res => {
-                  let dataResult = new ResponseListDTO(res);
-                  this._listConcepts = dataResult.data;
-              });
+                var dataResult: any = res;
+                this._listApplyTo = [];
+                for (var i = 0; i < dataResult.value.data.length; i++) {
+                    this._listApplyTo.push({
+                        "applyToId": dataResult.value.data[i].generalTableId,
+                        "name": dataResult.value.data[i].value
+                    });
+                }
+            });
+    }
+
+    getConceptByTypes(): void {
+        this.masterDataService.getConceptsByTypeIdList([31, 29])
+            .subscribe(res => {
+                let dataResult = new ResponseListDTO(res);
+                this._listConcepts = dataResult.data;
+            });
     }
 
     onCancelDetail() {
         this.eventoClose.emit(this.expenseDetailForm.get('expenseId').value);
+    }
+
+
+    isForAllTenant: boolean = false;
+    onApplyToChange() {
+        if (this.expenseDetailForm.get('applyTo').value === 66) {
+            this.isForAllTenant = true;
+            this.clearTenantIdValidator();
+        } else {
+            this.isForAllTenant = false;
+            this.assignTenantIdValidator();
+        }
+    }
+
+    assignTenantIdValidator() {
+        const tenantId = this.expenseDetailForm.get('tenantId');
+        tenantId.setValidators(Validators.required);
+        tenantId.updateValueAndValidity();
+    }
+
+    clearTenantIdValidator() {
+        const tenantId = this.expenseDetailForm.get('tenantId');
+        tenantId.clearValidators();
+        tenantId.updateValueAndValidity();
     }
 
 }
