6f9c324ec58373f3027093392cc748b372b15097
Paul Romero <jamromguz@outlook.com>
2019-07-25 00:22:34 -0500

    Agregando generacion de pdf y envio de correo

--- Amigo.Tenant.Application.Services.WebApi/Controllers/PaymentPeriodController.cs
+++ Amigo.Tenant.Application.Services.WebApi/Controllers/PaymentPeriodController.cs
@@ -21,6 +21,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Script.Serialization;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 
 namespace Amigo.Tenant.Application.Services.WebApi.Controllers
 {
@@ -64,12 +67,217 @@ namespace Amigo.Tenant.Application.Services.WebApi.Controllers
         {
             if (ModelState.IsValid)
             {
-                var response = await _paymentPeriodApplicationService.UpdatePaymentPeriodAsync(paymentPeriod);
-                return response;
+                //var response = await _paymentPeriodApplicationService.UpdatePaymentPeriodAsync(paymentPeriod);
+                await GenerateFileAndSend(paymentPeriod);
+                return null; // response;
             }
             return ModelState.ToResponse();
         }
 
+        private async Task GenerateFileAndSend(PPHeaderSearchByContractPeriodDTO paymentPeriod)
+        {
+            MemoryStream memStream = CrearDocumentoAmigo(paymentPeriod);
+            var attachment = new Attachment(new MemoryStream(memStream.ToArray()), System.Net.Mime.MediaTypeNames.Application.Pdf);
+            attachment.ContentDisposition.FileName = string.Format("Invoice_{0}.pdf", "Test");
+            await SendEmail(attachment, paymentPeriod);
+        }
+
+        private async Task SendEmail(Attachment attachment, PPHeaderSearchByContractPeriodDTO paymentPeriod)
+        {
+            var emailBody = "<!DOCTYPE html><html><head><title>Título de la WEB</title><meta charset='UTF-8'></head><body><h1>TEST H1</h1><div><table><tr><td>celda1</td></tr></table></div></body></html>";
+            var mail = new MailMessage
+            {
+                From = new MailAddress("pjromg@gmail.com"),
+                Subject = string.Format("Payment Notification - Renta AmigoTenant - Periodo: {0}", "2019"),
+                Body = emailBody,
+                IsBodyHtml = true
+            };
+            mail.Attachments.Add(attachment);
+            mail.To.Add("jamromguz@outlook.com");
+
+            var client = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new System.Net.NetworkCredential("pjromg@gmail.com", "jursaturnepplu"),
+                EnableSsl = true
+            };
+            await client.SendMailAsync(mail);
+        }
+
+        private MemoryStream CrearDocumentoEjemplo()
+        {
+            // Creamos el documento con el tamaño de página tradicional
+            Document doc = new Document(PageSize.LETTER);
+
+            MemoryStream memStream = new MemoryStream();
+            PdfWriter writer1 = PdfWriter.GetInstance(doc, memStream);
+
+            // Indicamos donde vamos a guardar el documento
+            //PdfWriter writer = PdfWriter.GetInstance(doc,
+            //                            new FileStream(@"C:\prueba.pdf", FileMode.Create));
+
+            // Le colocamos el título y el autor
+            // **Nota: Esto no será visible en el documento
+            doc.AddTitle("Mi primer PDF");
+            doc.AddCreator("Roberto Torres");
+
+            // Abrimos el archivo
+            doc.Open();
+
+            // Escribimos el encabezamiento en el documento
+            doc.Add(new Paragraph("Mi primer documento PDF"));
+            doc.Add(Chunk.NEWLINE);
+
+            iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+            // Creamos una tabla que contendrá el nombre, apellido y país 
+            // de nuestros visitante.
+            PdfPTable tblPrueba = new PdfPTable(3);
+            tblPrueba.WidthPercentage = 100;
+
+            // Configuramos el título de las columnas de la tabla
+            PdfPCell clNombre = new PdfPCell(new Phrase("Nombre", _standardFont));
+            clNombre.BorderWidth = 0;
+            clNombre.BorderWidthBottom = 0.75f;
+
+            PdfPCell clApellido = new PdfPCell(new Phrase("Apellido", _standardFont));
+            clApellido.BorderWidth = 0;
+            clApellido.BorderWidthBottom = 0.75f;
+
+            PdfPCell clPais = new PdfPCell(new Phrase("País", _standardFont));
+            clPais.BorderWidth = 0;
+            clPais.BorderWidthBottom = 0.75f;
+
+            // Añadimos las celdas a la tabla
+            tblPrueba.AddCell(clNombre);
+            tblPrueba.AddCell(clApellido);
+            tblPrueba.AddCell(clPais);
+
+            // Llenamos la tabla con información
+            clNombre = new PdfPCell(new Phrase("Roberto", _standardFont));
+            clNombre.BorderWidth = 0;
+
+            clApellido = new PdfPCell(new Phrase("Torres", _standardFont));
+            clApellido.BorderWidth = 0;
+
+            clPais = new PdfPCell(new Phrase("Puerto Rico", _standardFont));
+            clPais.BorderWidth = 0;
+
+            // Añadimos las celdas a la tabla
+            tblPrueba.AddCell(clNombre);
+            tblPrueba.AddCell(clApellido);
+            tblPrueba.AddCell(clPais);
+
+            clNombre = new PdfPCell(new Phrase("Juan", _standardFont));
+            clNombre.BorderWidth = 0;
+
+            clApellido = new PdfPCell(new Phrase("Rodríguez", _standardFont));
+            clApellido.BorderWidth = 0;
+
+            clPais = new PdfPCell(new Phrase("México", _standardFont));
+            clPais.BorderWidth = 0;
+
+            tblPrueba.AddCell(clNombre);
+            tblPrueba.AddCell(clApellido);
+            tblPrueba.AddCell(clPais);
+            // Finalmente, añadimos la tabla al documento PDF y cerramos el documento
+            doc.Add(tblPrueba);
+
+            //intento1
+            //memStream.Position = 0;
+
+
+            doc.Close();
+            writer1.Close();
+            return memStream;
+        }
+        private MemoryStream CrearDocumentoAmigo(PPHeaderSearchByContractPeriodDTO paymentPeriod)
+        {
+            // Creamos el documento con el tamaño de página tradicional
+            Document doc = new Document(PageSize.LETTER);
+
+            MemoryStream memStream = new MemoryStream();
+            PdfWriter writer1 = PdfWriter.GetInstance(doc, memStream);
+            //StringBuilder sb = CreateHtml();
+
+            // Le colocamos el título y el autor
+            // **Nota: Esto no será visible en el documento
+            doc.AddTitle("Mi primer PDF");
+            doc.AddCreator("Roberto Torres");
+
+            // Abrimos el archivo
+            doc.Open();
+
+            //// Escribimos el encabezamiento en el documento
+            doc.Add(new Paragraph("Mi primer documento PDF"));
+            doc.Add(Chunk.NEWLINE);
+
+            //string imagepath = System.Web.Hosting.HostingEnvironment.MapPath("~/Images/logo_invoice.png"); //Application.StartupPath;
+            Image jpg = Image.GetInstance(System.Web.Hosting.HostingEnvironment.MapPath("~/Images/logo_invoice.png"));
+            jpg.ScaleToFit(300f, 300f);
+            jpg.SpacingAfter = 12f;
+            jpg.SpacingBefore = 12f;
+            doc.Add(new Paragraph("GIF"));
+
+            //Image gif = Image.GetInstance(imagepath + "/mikesdotnetting.gif");
+
+            doc.Add(jpg);
+
+            iTextSharp.text.Font _standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+
+            // Creamos una tabla que contendrá el nombre, apellido y país 
+            // de nuestros visitante.
+            PdfPTable tblDetail = new PdfPTable(3);
+            tblDetail.WidthPercentage = 100;
+
+            // Configuramos el título de las columnas de la tabla
+            PdfPCell clSequence = new PdfPCell(new Phrase("No.", _standardFont));
+            clSequence.BorderWidth = 0;
+            clSequence.BorderWidthBottom = 0.75f;
+
+            PdfPCell clDescription = new PdfPCell(new Phrase("Description", _standardFont));
+            clDescription.BorderWidth = 0;
+            clDescription.BorderWidthBottom = 0.75f;
+
+            PdfPCell clAmount = new PdfPCell(new Phrase("Amount", _standardFont));
+            clAmount.BorderWidth = 0;
+            clAmount.BorderWidthBottom = 0.75f;
+
+            // Añadimos las celdas a la tabla
+            tblDetail.AddCell(clSequence);
+            tblDetail.AddCell(clDescription);
+            tblDetail.AddCell(clAmount);
+
+            // Llenamos la tabla con información
+            var sequence = 1;
+            paymentPeriod.PPDetail.ForEach(q => 
+            {
+                clSequence = new PdfPCell(new Phrase(sequence.ToString(), _standardFont));
+                clSequence.BorderWidth = 0;
+
+                clDescription = new PdfPCell(new Phrase(q.PaymentDescription, _standardFont));
+                clDescription.BorderWidth = 0;
+
+                clAmount = new PdfPCell(new Phrase(q.PaymentAmount.ToString(), _standardFont));
+                clAmount.BorderWidth = 0;
+
+                // Añadimos las celdas a la tabla
+                tblDetail.AddCell(clSequence);
+                tblDetail.AddCell(clDescription);
+                tblDetail.AddCell(clAmount);
+
+                sequence++;
+            });
+
+            // Finalmente, añadimos la tabla al documento PDF y cerramos el documento
+            doc.Add(tblDetail);
+            //memStream.Position = 0;
+
+
+            doc.Close();
+            writer1.Close();
+            return memStream;
+        }
 
         [HttpGet, Route("calculateLateFeeByContractAndPeriod")]
         public async Task<ResponseDTO<PPDetailSearchByContractPeriodDTO>> CalculateLateFeeByContractAndPeriod([FromUri]PaymentPeriodSearchByContractPeriodRequest search)
